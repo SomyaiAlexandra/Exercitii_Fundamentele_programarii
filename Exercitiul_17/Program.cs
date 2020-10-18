b10a40836076d07fc5f88085dd5ebe533f0dd005
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 18:23:59 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sa se scrie un program care determina aria totala a unui cub pentru care se cunoaste lungimea laturii.
+
+            Console.WriteLine("Introduceti latura cubului");
+
+            int latura = int.Parse(Console.ReadLine());
+            double Arie = Math.Pow(latura, 2) * 6;
+
+            Console.WriteLine("Aria este egala cu"); Console.WriteLine(Arie);
+            Console.ReadKey();
+
+        }
+    }
+}
