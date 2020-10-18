2c66452e09d5bff3aefb92b93e1e2a7b849b9162
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 10:59:06 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (#941) Scrie un program care afişează pe ecran o urare pentru cei dragi ţie!
+
+            Console.WriteLine("Sarbatori fericite!");
+        }
+    }
+}
