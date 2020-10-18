eafd2cbd9cd50adec4aba7f8b83aaa46f3339e5d
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 10:01:16 +0300

    Am finalizat cu succes exercitiul.

---
+++ Execitiul 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execitiul_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // (#939)Se dau 2 numere naturale. Calculaţi suma celor 2 numere date.
+        Console.WriteLine("Introduceti doua numere naturale");
+        int a, b;
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Suma celor doua numere este...");
+
+        Console.WriteLine(a + b);
+
+            Console.ReadKey();
+
+
+    }
+}
+}
