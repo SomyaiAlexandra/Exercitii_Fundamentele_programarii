295ba6216cad0d963a9c0dd7c8a83f90934e1a7c
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 10:21:20 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (#167) Să se scrie un program care citeşte de la tastatură un număr întreg şi determină semnul său.
+
+            Console.WriteLine("Introduceti un numar intreg");
+
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            if (n > 0) Console.WriteLine("Numarul e POZITIV");
+
+            if (n < 0) Console.WriteLine("Numarul e NEGATIV");
+            
+            if (n == 0) Console.WriteLine("Numarul e NUL");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
