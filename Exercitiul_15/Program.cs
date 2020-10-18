70b3d8476354483ee9cf7c1986b31698fdb17909
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 18:05:09 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calculati media aritmetica a doua numere intregi.
+
+            Console.WriteLine("Introduceti doua numere");
+            double a, b;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Media aritmetica a numerelor este...");
+            double mediearitmetica = (a + b) / 2;
+            Console.WriteLine(mediearitmetica);
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
