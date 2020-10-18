ac38497f92986662410484071d6b8fc9c310e747
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 17:47:53 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se dat trei numere naturale a,b,x. Sa se verifice daca numarul x apartine intervalului [a,b].
+
+            int a, b, x;
+            Console.WriteLine("Introduceti cel mai mic numar al intervalului");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Intoduceti cel mai mare numar al intervalului");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti numarul x");
+            x = int.Parse(Console.ReadLine());
+
+            if ((x >= a) && (x <= b))
+                Console.WriteLine("Numarul x apartine intervalului");
+            else
+                Console.WriteLine("Numarul x nu apartine intervalului");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
