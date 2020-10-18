1aa550225881373ec2a8934aec52dabace780e5b
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 18:47:41 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Se citesc doua numere a si b reprezentand lungimile laturilor unui dreptungi.
+            // Sa se calculeze perimetrul, aria si lungimea diagonalei dreptunghiului.
+
+            Console.WriteLine("Introdeceti lungimea laturii");
+            int L = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Intoduceti latimea laturii");
+            int l = int.Parse(Console.ReadLine());
+
+            int Perimetru = (L + l) * 2;
+            Console.WriteLine($"Perimetrul dreptunghiului este {Perimetru}");
+
+            int Arie = L * l;
+            Console.WriteLine($"Aria dreptunghiului este {Arie}");
+            
+            int d = (int)Math.Sqrt(L * L + l * l);
+            int Diagonala = (int)Math.Pow(d, 2);
+            Console.WriteLine($"Diagonala dreptunghiului este {Diagonala}");
+
+            Console.ReadKey();
+            
+            
+
+        }
+    }
+}
