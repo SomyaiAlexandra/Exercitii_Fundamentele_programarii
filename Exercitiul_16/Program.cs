08f8ca74d140be7b6b8b55e8cb4a6c30685ffe35
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 18:08:36 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calculati media geometrica a doua numere.
+
+            Console.WriteLine("Introduceti doua numere");
+            double a, b;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Media geometrica a numerelor este...");
+            double mediegeometrica = Math.Sqrt(a*b);
+            Console.WriteLine(mediegeometrica);
+
+            Console.ReadKey();
+        }
+    }
+}
