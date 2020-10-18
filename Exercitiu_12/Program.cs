66b13d3ac02745a67d486b739d028234c25bea24
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 17:39:14 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiu_12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiu_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sa se scrie un program care determina maximul a doua numere intregi citite de la tastatura.
+            
+            Console.WriteLine("Introduceti doua numere intregi");
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numarul maxim este...");
+            if (a > b)
+                Console.WriteLine(a);
+            else
+                Console.WriteLine(b);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
