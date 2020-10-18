ce2c3f3c0c13dde7aed022ae972ec266b39ebfc8
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 16:44:42 +0300

    Am finalizat cu succses exercitiul.

---
+++ Exercitiul_9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (#1282) Se dă un număr. Să se afișeze rădăcina sa pătrată.
+
+            Console.WriteLine("Introduceti un numar n");
+            int n;
+            n = int.Parse(Console.ReadLine());
+            double r = Math.Sqrt(n);
+
+            Console.WriteLine("Radacina patrata a numarului este...");
+            Console.WriteLine(r); 
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
