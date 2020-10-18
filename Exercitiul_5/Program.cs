03ffff77bbace9290db6bd518248ffb4d325d359
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 10:55:13 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (#109) Să se scrie un program care citeşte de la tastatură un număr natural şi verifică dacă numărul este par sau impar.
+
+            Console.WriteLine("Introduceti un numar natural");
+
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0) Console.WriteLine("Numarul este PAR");
+
+            if (n % 2 == 1)Console.WriteLine("Numarul este IMPAR");
+
+            Console.ReadKey();
+
+        
+        
+        
+        }
+    }
+}
