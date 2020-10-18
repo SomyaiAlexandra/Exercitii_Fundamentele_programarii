08d04571de59b43c3737185395fd004ff6e4b9ec
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 17:57:55 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calculati aria unui triunghi.
+            Console.WriteLine("Intoduceti cele 3 laturi ale triunghiului");
+
+            double a, b, c;
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+
+            double semiperimetru = (a + b + c) / 2;
+            double aria = (double)Math.Sqrt(semiperimetru * (semiperimetru - a) * (semiperimetru - b) * (semiperimetru - c));
+
+            Console.WriteLine("Aria este egala cu...");
+            Console.WriteLine(aria);
+            Console.ReadKey();
+
+
+        }
+    }
+}
