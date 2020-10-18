aa06284956a6b6a748b442a226cfec783d5d2620
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-18 10:32:26 +0300

    Am finalizat cu succes exercitiul.

---
+++ Exercitiul_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercitiul_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (#168) Să se scrie un program care citeşte de la tastatură două numere întregi a şi b şi verifică dacă cele doua numere au acelaşi semn.
+
+            Console.WriteLine("Intoduceti doua numere intregi");
+
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            if (a > 0 && b > 0) Console.WriteLine("DA! Ambele numere sunt pozitive");
+            if (a < 0 && b < 0) Console.WriteLine("DA! Ambele numere sunt negative");
+            if (a < 0 && b > 0) Console.WriteLine("NU! Numerele nu au acelasi semn");
+            if (a > 0 && b < 0) Console.WriteLine("NU! Numerele nu au acelasi semn");
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
