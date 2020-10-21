714656831f06d7d643b40ab213d8f8da47951092
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-21 19:09:54 +0300

    Problema 1_sume

---
+++ Problema_sume_produse_numarari/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_sume_produse_numarari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (#1360)Se citește n număr natural. Calculați suma tuturor numerelor 
+            // naturale mai mici sau egale cu n.
+
+            Console.WriteLine("Introduceti nu numar natural n");
+
+            float n = float.Parse(Console.ReadLine());
+
+            float Gauss = n*(n + 1) / 2;
+
+            Console.WriteLine("Suma este egala cu ...");
+
+            Console.WriteLine(Gauss);
+
+            Console.ReadKey();
+
+        }
+    }
+}
