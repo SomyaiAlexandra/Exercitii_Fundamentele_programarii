e89cd51175a38cd66ffedc6f7ff04c04a93f73b1
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-21 19:43:13 +0300

    Nefinalizat

---
+++ Problema_3_sume_produse_numarari/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3_sume_produse_numarari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Somyai Alexandra
+            // (#332) Să se scrie un program care citește numărul natural n 
+            // și determină suma S=1*2+2*3+3*4+...+n*(n+1).
+
+            Console.WriteLine("Introduceti un numar natural n");
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Suma numerelor este...");
+
+            S = 
+
+            Console.WriteLine("S");
+
+
+        }
+    }
+}
