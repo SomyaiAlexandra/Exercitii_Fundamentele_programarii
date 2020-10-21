802f2e0486db7821630eecc91ce52a89d48c58a6
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-21 19:32:41 +0300

    Am finalizat exercitiul.

--- Problema_1_sume_produse_numarari/Program.cs
+++ Problema_1_sume_produse_numarari/Program.cs
@@ -28,3 +28,4 @@ namespace Problema_sume_produse_numarari
         }
     }
 }
+           
---
+++ Problema_2_sume_produse_numarari/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2_sume_produse_numarari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // (#46) Să se scrie un program care citește numărul natural n 
+            // și determină suma S=2+4+..+(2n).
+
+            Console.WriteLine("Introduceti un numar natural");
+
+            
+            int n = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Suma este egala cu...");
+
+            int S =  n * (n + 1);
+
+            Console.WriteLine(S);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
