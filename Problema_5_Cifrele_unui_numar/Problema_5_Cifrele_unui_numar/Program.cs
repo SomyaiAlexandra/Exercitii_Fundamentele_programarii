eb8915d1343a9133c7652e241bf713db2c82ca1a
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-27 00:03:05 +0200

    Finish code.

---
+++ Problema_5_Cifrele_unui_numar/Problema_5_Cifrele_unui_numar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_5_Cifrele_unui_numar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Somyai Alexandra
+            // Calculati suma cifrelor unui numar dat.
+
+            Console.WriteLine("Introduceti un numar n");
+
+            int S = 0;
+            int n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                S = S + n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine("Suma este egala cu {0}", S);
+            Console.ReadKey();
+
+
+        }
+    }
+}
