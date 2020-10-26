7d6a27910c0867445f4c8240a6784a593ffd2d31
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-26 22:17:56 +0200

    Finish code.

---
+++ Problema_4_sume_produse_numarari/Problema_4_sume_produse_numarari/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_4_sume_produse_numarari
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Somyai Alexandra
+			// (#49) Să se scrie un program care citește numărul natural n și determină valoarea lui n!.
+
+			Console.WriteLine("Introduceti un numar natural n...");
+			int n = int.Parse(Console.ReadLine());
+			int p = 1;
+
+			for (int i = 1; i <= n; i++) 
+			p *= i;
+
+			Console.WriteLine("n! este {0}", p);
+			Console.ReadKey();
+
+
+
+		}
+	}
+}
