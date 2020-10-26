302805ea332f4d271b40efe520ccd374023185c4
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-26 23:49:31 +0200

    Finish code.

---
+++ Problema_2_Cifrele_unui_numar/Problema_2_Cifrele_unui_numar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2_Cifrele_unui_numar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Somyai Alexandra
+            // Intoduceti un numar n de la tastatura si sa se arate cate cifre impare are numarul.
+
+            
+
+            int n = int.Parse(Console.ReadLine());
+            int m;
+            int imp = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                m = int.Parse(Console.ReadLine());
+
+                if (m % 2 == 1)
+                    imp++;
+            }
+            if (imp == 1)
+            
+                Console.WriteLine("Nu exista cifre pare");
+            
+            else
+                Console.WriteLine(imp);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Problema_3_Cifrele_unui_numar/Problema_3_Cifrele_unui_numar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3_Cifrele_unui_numar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Somyai Alexandra
+            // Se da un numar natural n. Sa se calculeze produsul primelor 2 cifre ale numarului n.
+
+            int P = 1;
+            int n = int.Parse(Console.ReadLine());
+
+            while (n > 100)
+                n = n / 10;
+            while (n != 0)
+            {
+                P = P * (n % 10);
+                n = n / 10;
+            }
+            Console.WriteLine("Produsul este {0}", P);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
