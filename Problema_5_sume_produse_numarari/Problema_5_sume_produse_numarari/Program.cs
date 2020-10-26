5e3bebdd76be9331a2fac9ca55629e768f3de613
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-26 22:39:52 +0200

    Finish code.

--- Problema_5_sume_produse_numarari/Problema_5_sume_produse_numarari/Program.cs
+++ Problema_5_sume_produse_numarari/Problema_5_sume_produse_numarari/Program.cs
@@ -11,7 +11,21 @@ namespace Problema_5_sume_produse_numarari
 		static void Main(string[] args)
 		{
 			// Somyai Alexandra
-			// (#) 
+			// (#338) Să se scrie un program care citește numărul natural n 
+			//  și determină suma S=1^1+2^2+ ... + n^n.
+
+			Console.WriteLine("Introduceti un numar natural");
+
+			int n = int.Parse(Console.ReadLine());
+			int S = 0;
+
+			for (int i = 1; i <= n; i++)
+
+				S += i * i;
+
+			Console.WriteLine("Suma este egala cu {0} ", S);
+			Console.ReadKey();
+
 
 
 		}
